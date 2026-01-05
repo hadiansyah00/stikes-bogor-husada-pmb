6cc6e0b390c4bd67ca3256b8ca18a7f0bcc2874c
masesih <97597512+hadiansyah00@users.noreply.github.com>
2026-01-05 11:25:01 +0700

    a

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,15 @@
 export default defineNuxtConfig({
-  ssr: true, // Set ke false jika hanya ingin SPA
+  ssr: true,
+
   app: {
-    // Properti baseURL Anda yang sudah ada, JANGAN DIHAPUS
     baseURL: "/",
 
-    // Tambahkan properti 'head' di sini, di dalam 'app'
     head: {
-      // ... title, meta, dan link Anda yang lain bisa ditambahkan di sini
-
       script: [
         {
+          hid: "meta-pixel",
           innerHTML: `
-           !function(f,b,e,v,n,t,s)
+            !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -21,19 +19,17 @@ export default defineNuxtConfig({
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '3852898454807503');
             fbq('track', 'PageView');
-            fbq('track', 'Lead');
-            fbq('track', 'CompleteRegistration');
           `,
-          body: true,
           type: "text/javascript",
           charset: "utf-8",
         },
       ],
+
       noscript: [
         {
+          hid: "meta-pixel-noscript",
           innerHTML:
             '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=3852898454807503&ev=PageView&noscript=1" />',
-          body: true,
         },
       ],
     },
@@ -41,9 +37,6 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      googleScriptUrl: process.env.GOOGLE_SCRIPT_URL || "",
-      waApiKey: process.env.WA_API_KEY || "",
-      waNumberKey: process.env.WA_NUMBER_KEY || "",
       apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || "https://sbh.ac.id",
       imageBaseUrl:
         process.env.NUXT_PUBLIC_IMAGE_BASE_URL || "https://sbh.ac.id",
@@ -52,23 +45,8 @@ export default defineNuxtConfig({
 
   modules: ["@nuxtjs/tailwindcss"],
 
-  tailwindcss: {
-    viewer: false, // Nonaktifkan Tailwind viewer di build
-  },
-
-  vite: {
-    css: {
-      devSourcemap: process.env.NODE_ENV === "development", // Hanya aktif di dev mode
-    },
-
-    server: {},
-  },
-
-  build: {
-    transpile: [], // Jika perlu transpile lib tertentu, masukkan di sini
-  },
   devServer: {
     port: 3001,
-    host: "0.0.0.0", // opsional, jika ingin diakses dari IP lokal/jaringan
+    host: "0.0.0.0",
   },
 });
